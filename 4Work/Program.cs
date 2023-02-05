7e4df7688e04a1d31403ef9b2f51575bf8038a01
Kretyg <sokolovsky.zhora58@gmail.com>
2023-02-05 14:55:03 +0300

    add 4 work

--- 4Work/Program.cs
+++ 4Work/Program.cs
@@ -1,15 +1,15 @@
 ﻿// Написать программу копирования массива
 
 Console.Write("- Введите количество элементов массива: ");
-int length = Convert.ToInt32(Console.ReadLine());
+int length = int.Parse(Console.ReadLine());
 Console.WriteLine(""); 
 
 Console.Write("Массив: "); 
 double[] array = new double[length];
-Random rand = new Random(DateTime.Now.Millisecond);
+Random rand = new Random();
 for (int i = 0; i < array.Length; i++)
 {
-    array[i] = Math.Round((rand.NextDouble() * 200 - 100), 2);
+    array[i] = rand.Next(10);
     Console.Write(array[i]);
     if (i < length - 1 ) Console.Write("; ");
 }
